b185ea21330e3410e78e32ce16493dcdcebcb258
Amrish Kumar <amrish.kumar@leadschool.in>
2021-09-11 20:54:27 +0530

    Added first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
-function App() {
+const getRandomCountry = (countries) => countries[Math.floor(Math.random()*countries.length)];
+
+const getNeighbors = async (countryName) => {
+  const neighboursResponse = await fetch(`https://travelbriefing.org/${countryName}?format=json`);
+  const {names, neighbors} = await neighboursResponse.json();
+  return {name: names.name, neighbors};
+}
+
+const getGroupings = (neighbors, countries) => {
+  const groupings = {};
+  neighbors.forEach(n => {
+    n.neighbors.forEach( nc => {
+      if(countries.includes(nc.name)) {
+        if(nc.name > n.name) groupings[n.name] = nc.name;
+        else groupings[nc.name] = n.name;
+      }
+    })
+  });
+
+  return groupings;
+}
+
+const App = () => {
+  const [randomCountries, setRandomCountries] = useState(null);
+  const [neighbors, setNeighbors] = useState(null);  // [{name: 'usa', neighbors: []}
+  const [groupings, setGroupings] = useState(null);  // { canada: usa, austrailia: mexico}
+
+  useEffect(() => {
+    const getRandomCountriesAndNeighbors = async () => {
+      const countriesResponse = await fetch('https://travelbriefing.org/countries.json');
+      const countries = await countriesResponse.json();
+
+      let randomCountries = [];
+      while(randomCountries.length < 10) {
+        const randomCountry = getRandomCountry(countries);
+        if(!randomCountries.includes(randomCountry.name)) {
+          randomCountries.push(randomCountry.name);
+        }
+      }
+
+      const promises = randomCountries.map(async country => {
+        return getNeighbors(country);
+      })
+
+      Promise.all(promises)
+        .then(values => {
+          setNeighbors(values);
+          setRandomCountries(randomCountries);
+        })
+    }
+
+    getRandomCountriesAndNeighbors();
+  }, []);
+
+  if(!neighbors) return <div>Loading...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: 'left'}}>
+      <button 
+        onClick={() => {
+        const groupings = getGroupings(neighbors, randomCountries);
+        setGroupings(groupings);
+      }}>
+        Generate groupings
+      </button>
+      <h1>Selected countries</h1>
+      <ul>
+       {
+         neighbors.map(neighbor => {
+           return <li>{neighbor.name}</li>
+         })
+       }
+       </ul>
+       <h1>Neighbours</h1>
+       {
+         groupings ? 
+          ((Object.keys(groupings).length) ? 
+              <>{Object.keys(groupings).map(group => {
+                return <div style={{ display: 'flex'}}>
+                  <span>{group}</span>
+                  &nbsp;
+                  <span>
+                    {groupings[group]}
+                  </span>
+                </div>
+              })}</> : <div>No groupings found</div>) 
+          :<div>Click on generate groupings to find neighbors</div>
+        }
     </div>
   );
 }
